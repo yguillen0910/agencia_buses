29b297714c494d3020e1c9aef59d73f23d01401c
Yuzmhar Guillen <yuzmhar.guillen@elemetrica.com>
2019-03-06 01:35:31 -0300

    Cambios para usar props en componente en la vista de trayecto

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -5,36 +5,18 @@
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
 export default {
   name: "Table",
-  mounted() {
-    this.$store.dispatch("obtenerTrayectos");
+  props: {
+    trayectos: {
+      type: Array
+    },
+    fields: {
+      type: Object
+    }
   },
   data() {
-    return {
-      fields: {
-        Nombre: {
-          label: "Trayecto",
-          sortable: true
-        },
-        CiudadSalida: {
-          label: "Ciudad Salida",
-          sortable: true
-        },
-        CiudadDestino: {
-          label: "Ciudad Destino",
-          sortable: true
-        },
-        Promedio: {
-          label: "Promedio de Pasajeros",
-          sortable: true
-        }
-      }
-    };
-  },
-  computed: {
-    ...mapState(["trayectos"])
+    return {};
   }
 };
 </script>
--- src/views/Trayecto.vue
+++ src/views/Trayecto.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header></Header>
-    <Table></Table>
+    <Table v-bind:trayectos="trayectos" v-bind:fields="fields"></Table>
   </div>
 </template>
 
@@ -10,11 +10,40 @@
 import Header from "@/components/Header.vue";
 import Table from "@/components/Table.vue";
 
+import { mapState, mapActions } from "vuex";
 export default {
   name: "trayecto",
   components: {
     Header,
     Table
+  },
+  mounted() {
+    this.$store.dispatch("obtenerTrayectos");
+  },
+  data() {
+    return {
+      fields: {
+        Nombre: {
+          label: "Trayecto",
+          sortable: true
+        },
+        CiudadSalida: {
+          label: "Ciudad Salida",
+          sortable: true
+        },
+        CiudadDestino: {
+          label: "Ciudad Destino",
+          sortable: true
+        },
+        Promedio: {
+          label: "Promedio de Pasajeros",
+          sortable: true
+        }
+      }
+    };
+  },
+  computed: {
+    ...mapState(["trayectos"])
   }
 };
 </script>
